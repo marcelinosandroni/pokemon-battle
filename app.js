234e0c8b9668927fa8cac9f6a770395e49805229
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-02-24 19:22:32 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body')
+const container = document.querySelector('.container')
+
+// classe la no css: fundao
+
+
+container.classList.add('fundao')
+
+const battleContainer = document.createElement()
